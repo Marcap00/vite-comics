71f9443d3b53906b338c3db3377eacc579ea4575
Marcap00 <cappellatomarco@yahoo.com>
2024-09-17 15:41:15 +0200

    added components for cards

---
+++ src/components/MainListCards.vue
@@ -0,0 +1,21 @@
+<script>
+import CardsItem from './ListCardsItem.vue';
+
+export default {
+    data() {
+        return {
+
+        }
+    },
+    components: {
+        CardsItem
+    }
+}
+</script>
+
+<template>
+    <h2>Cards</h2>
+    <CardsItem />
+</template>
+
+<style lang="scss" scoped></style>
