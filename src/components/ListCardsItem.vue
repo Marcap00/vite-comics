0482f2bd492204a3584f15eeb9af3a6ff1ee68fb
Marcap00 <cappellatomarco@yahoo.com>
2024-09-17 17:54:59 +0200

    added dynamic cards comics

--- src/components/ListCardsItem.vue
+++ src/components/ListCardsItem.vue
@@ -4,12 +4,66 @@ export default {
         return {
 
         }
+    },
+    props: {
+        thumbUrl: {
+            type: String,
+            required: true
+        },
+        price: {
+            type: String,
+            required: false
+        },
+        titleSeries: {
+            type: String,
+            required: true
+        },
+        type: {
+            type: String,
+            required: false
+        }
     }
 }
 </script>
 
 <template>
-    <h2>Card</h2>
+
+    <div class="col">
+        <div class="card">
+
+            <div class="card-img">
+                <img class="card-img" :src="thumbUrl" :alt="titleSeries">
+            </div>
+
+            <div class="card-body">
+                <h5 class="card-title">
+                    {{ titleSeries.toUpperCase() }}
+                </h5>
+            </div>
+
+        </div>
+    </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.card-img {
+    max-height: 150px;
+    width: 150px;
+    object-fit: cover;
+    object-position: top;
+    margin-bottom: 1rem;
+}
+
+.card-body {
+    max-width: 150px;
+
+    text-align: left;
+    color: #fff;
+
+    h5 {
+        font-weight: 600;
+        margin-bottom: 1rem;
+    }
+
+}
+</style>
